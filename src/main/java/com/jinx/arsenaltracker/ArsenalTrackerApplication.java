96daae33ccf22f82fac101cf65b334c2e6b15fd3
Abdus Samee <sameeabdus94@gmail.com>
2026-06-24 16:57:38 +0530

    Initial commit: Spring Boot project setup with hello endpoint

---
+++ src/main/java/com/jinx/arsenaltracker/ArsenalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jinx.arsenaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArsenalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArsenalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinx/arsenaltracker/ArsenalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinx.arsenaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsenalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
